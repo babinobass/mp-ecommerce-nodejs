308c3784bbbe8340c4acd4eea3420304aeffdeea
babinobass <69868198+babinobass@users.noreply.github.com>
2021-07-30 06:46:58 -0300

    a

--- app.js
+++ app.js
@@ -1,22 +1,94 @@
+require('dotenv').config();
 var express = require('express');
-var exphbs  = require('express-handlebars');
-var port = process.env.PORT || 3000
+var exphbs = require('express-handlebars');
+const { data } = require('./utils/mock')
+const mercadopago = require('mercadopago');
+
+mercadopago.configure({
+  access_token: "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398"
+});
+
+
+var port = process.env.PORT || 3001
+const host = process.env.HOST || "http://localhost:3001/"
 
 var app = express();
- 
+
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.use(express.static('assets'));
- 
+
 app.use('/assets', express.static(__dirname + '/assets'));
 
 app.get('/', function (req, res) {
-    res.render('home');
+  res.render('home');
 });
 
 app.get('/detail', function (req, res) {
-    res.render('detail', req.query);
+  let pref = data(req.query, host)
+  mercadopago.preferences.create(pref)
+    .then(function (response) {
+      req.query.preferenceId = response.body.id;
+      console.log(response.body)
+      res.render("detail", req.query);
+    }).catch(function (error) {
+      console.log(error);
+    });
+});
+
+app.get("/ipn", function (req, res) {
+  console.log(req);
+  console.log(res);
+  res.send("Received IPN");
+});
+
+app.get("/success", function (req, res) {
+  console.log(req);
+  res.json("success");
+});
+
+app.get("/pending", function (req, res) {
+  res.json("pending");
+});
+
+app.get("/failure", function (req, res) {
+  console.log(req);
+  res.json("failure");
+});
+
+app.post("/pay", function (req, res) {
+  console.log(req.body);
+  console.log(req.query);
+
+  const payment_id = req.body.payment_id;
+  const merchant_order_id = req.body.merchant_order_id;
+  const status = req.body.payment_status;
+
+  switch (status) {
+    case "approved":
+      res.render("success", {
+        payment_id: payment_id,
+        merchant_order_id: merchant_order_id
+      });
+      break;
+    case "pending":
+      res.render("success", {
+        payment_id: payment_id,
+        merchant_order_id: merchant_order_id
+      });
+      break;
+    case "error":
+      res.render("success", {
+        payment_id: payment_id,
+        merchant_order_id: merchant_order_id
+      });
+      break;
+    default:
+      res.status(404).json({ error: "Faltan los parametros de POST" });
+      break;
+  }
 });
 
 app.listen(port);
---
+++ utils/mock.js
@@ -0,0 +1,62 @@
+
+function data({ title, img, price, unit }, host) {
+
+    let preference = {
+        items: [
+            {
+                id: "1234",
+                title: String(title),
+                description: "Dispositivo móvil de Tienda e-commerce",
+                picture_url: `${host}${img}`,
+                unit_price: Number(price),
+                quantity: Number(unit)
+            }
+        ],
+        payer: {
+            name: "Lalo",
+            surname: "Landa",
+            email: "test_user_63274575@testuser.com",
+            date_created: "2015-06-02T12:58:41.425-04:00",
+            phone: {
+                area_code: "11",
+                number: 22223333
+            },
+
+            identification: {
+                type: "DNI",
+                number: "12345678"
+            },
+
+            address: {
+                street_name: "Falsa",
+                street_number: 123,
+                zip_code: "1111"
+            }
+        },
+        back_urls: {
+            success: `${host}success`,
+            failure: `${host}failure`,
+            pending: `${host}pending`
+        },
+        auto_return: "approved",
+        payment_methods: {
+            excluded_payment_methods: [
+                {
+                    id: "amex"
+                }
+            ],
+            excluded_payment_types: [
+                {
+                    id: "atm"
+                }
+            ],
+            installments: 6
+        },
+        notification_url: `${host}ipn`,
+        external_reference: "sala.eliezer@gmail.com"
+    };
+    return preference
+}
+module.exports = {
+    data
+}
